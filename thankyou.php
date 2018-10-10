5948e87540fd8128db317db04e459cfe66ade1da
gitmasterhariom <43899492+gitmasterhariom@users.noreply.github.com>
2018-10-10 23:05:30 +0530

    Add files via upload

---
+++ thankyou.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+setTimeout("window.location='http://www.abacleocounty.com/'",500);
+</script>
+
+
+
+<h2>Thanks for contacting us!<br>One of our Consultants will get back to you within 24 hours</h2>
